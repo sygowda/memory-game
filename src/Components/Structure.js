41fba0acfa166c8db94737ea8b9dfe3237790ab8
Shubham Gowda <55272561+sygowda@users.noreply.github.com>
2021-05-14 23:17:11 -0700

    initial commit

--- src/Components/Structure.js
+++ src/Components/Structure.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "../Styles/Structure.css";
-import Result from '../Components/Result';
-import Card from '@material-ui/core/Card';
+import Result from "../Components/Result";
+import Card from "@material-ui/core/Card";
 import { Button, CardMedia } from "@material-ui/core";
 
 class Structure extends React.Component {
@@ -10,26 +10,12 @@ class Structure extends React.Component {
     let total = props.col * props.row;
     var arr = new Array(total);
     var suit = ["H", "D", "S", "C"];
-    var v = [
-      "A",
-      "2",
-      "3",
-      "4",
-      "5",
-      "6",
-      "7",
-      "8",
-      "9",
-      "0",
-      "J",
-      "K",
-      "Q"
-    ];
+    var v = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "0", "J", "K", "Q"];
     let i = 0;
-    while (i < total){
+    while (i < total) {
       let t1 = Math.floor(Math.random() * 4);
       let t2 = Math.floor(Math.random() * 13);
-      if (!arr.includes(v[t2] + suit[t1])){
+      if (!arr.includes(v[t2] + suit[t1])) {
         arr[i] = v[t2] + suit[t1];
         arr[i + 1] = v[t2] + suit[t1];
         i = i + 2;
@@ -37,14 +23,16 @@ class Structure extends React.Component {
     }
     arr = this.shuffle(arr);
     this.state = {
-      isCardFlippedArr: new Array(props.row * props.col).fill().map(() => true),
+      isCardFlippedArr: new Array(props.row * props.col)
+        .fill()
+        .map(() => false),
       isflipOnClickEnabled: new Array(props.row * props.col)
         .fill()
         .map(() => true),
       successfulCards: [],
       lastSelectedCard: "",
       arr: arr,
-      totalFlips : 0,
+      totalFlips: 0,
       successfulFlips: 0,
       hintUse: 3,
       w: window.innerWidth / 2,
@@ -83,40 +71,41 @@ class Structure extends React.Component {
 
   handleClick = () => {
     let hintUseCopy = this.state.hintUse;
-    if (hintUseCopy > 0){
-        let isCardFlippedArrCopy = this.state.isCardFlippedArr;
-        let temp = new Array(this.state.row * this.state.col).fill().map(() => false);
+    if (hintUseCopy > 0) {
+      let isCardFlippedArrCopy = this.state.isCardFlippedArr;
+      let temp = new Array(this.state.row * this.state.col)
+        .fill()
+        .map(() => true);
+      this.setState({
+        isCardFlippedArr: temp,
+        hintUse: hintUseCopy - 1
+      });
+      setTimeout(() => {
         this.setState({
-          isCardFlippedArr: temp,
-          hintUse: hintUseCopy - 1
-
-        })
-        setTimeout(() => {
-          this.setState({
-            isCardFlippedArr: isCardFlippedArrCopy
-          });}, 1000);
+          isCardFlippedArr: isCardFlippedArrCopy
+        });
+      }, 1000);
     }
-  }
+  };
 
   cardClicked = (element, index) => {
-    
     if (this.state.lastSelectedCard === "") {
       let copy = [...this.state.isCardFlippedArr];
-      copy[index] = false;
+      copy[index] = true;
       this.setState({
         lastSelectedCard: element + "/" + index,
         isCardFlippedArr: copy,
-        totalFlips: this.state.totalFlips+1
+        totalFlips: this.state.totalFlips + 1
       });
     } else if (this.state.lastSelectedCard.split("/")[0] === element) {
       let isflipOnClickEnabledCopy = [...this.state.isflipOnClickEnabled];
       let isCardFlippedArrCopy = [...this.state.isCardFlippedArr];
       let successfulCardsCopy = [...this.state.successfulCards];
       isflipOnClickEnabledCopy[index] = false;
-      isCardFlippedArrCopy[index] = false;
+      isCardFlippedArrCopy[index] = true;
       let previousElementIndex = this.state.lastSelectedCard.split("/")[1];
       isflipOnClickEnabledCopy[previousElementIndex] = false;
-      isCardFlippedArrCopy[previousElementIndex] = false;
+      isCardFlippedArrCopy[previousElementIndex] = true;
       successfulCardsCopy.push(parseInt(previousElementIndex), index);
       this.setState({
         isflipOnClickEnabled: isflipOnClickEnabledCopy,
@@ -130,76 +119,91 @@ class Structure extends React.Component {
       let isCardFlippedArrCopy = [...this.state.isCardFlippedArr];
       // console.log(isCardFlippedArrCopy0
       isCardFlippedArrCopy[index] = true;
-      let previousElementIndex = this.state.lastSelectedCard.split("/")[1];
-      console.log(previousElementIndex);
-      isCardFlippedArrCopy[previousElementIndex] = true;
-      // console.log(isCardFlippedArrCopy);
       this.setState({
-        lastSelectedCard: "",
-        isCardFlippedArr: isCardFlippedArrCopy,
-        totalFlips: this.state.totalFlips + 1
+        isCardFlippedArr: isCardFlippedArrCopy
       });
+      // this.UNSAFE_componentWillUpdate();
+      setTimeout(() => {
+        let previousElementIndex = this.state.lastSelectedCard.split("/")[1];
+        // console.log(previousElementIndex);
+        isCardFlippedArrCopy[previousElementIndex] = false;
+        isCardFlippedArrCopy[index] = false;
+        console.log(isCardFlippedArrCopy);
+        // console.log(isCardFlippedArrCopy);
+        this.setState({
+          lastSelectedCard: "",
+          isCardFlippedArr: isCardFlippedArrCopy,
+          totalFlips: this.state.totalFlips + 1
+        });
+      }, 1000);
     }
   };
 
   render() {
-    if(this.state.successfulCards.length === (this.state.row*this.state.col)){
-      return(
-        <Result total = {this.state.totalFlips/2} success = {this.state.successfulFlips}/>
-      )
+    if (this.state.successfulCards.length === this.state.row * this.state.col) {
+      return (
+        <Result
+          total={this.state.totalFlips / 2}
+          success={this.state.successfulFlips}
+        />
+      );
     }
     return (
       <div>
-        <Button variant = 'contained' color = 'primary' onClick = {() => {this.handleClick()}}> Hint </Button>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => {
+            this.handleClick();
+          }}
+        >
+          {" "}
+          Hint{" "}
+        </Button>
         <h3> Hints available : {this.state.hintUse}</h3>
-        <div className = "grid" style = {{
-            width : this.state.w,
-            height : this.state.h
-        }}>
-        {
-            
-            this.state.arr.map((element, i) => {
-              var dis = "";
-              var img_link = "https://deckofcardsapi.com/static/img/"+element+".png";
-              if(this.state.isCardFlippedArr[i]){
-                  return (
-                    <Card onClick={() => this.cardClicked(element, i)}
-                      style={{
-                        width:  this.state.w / this.state.col - 30,
-                        float: "left",
-                        margin: "1%",
-                        height:  this.state.h / this.state.col - 30 , 
-                        display: dis
-                      }}
-                    >
-                    </Card>
-                  
-                  )
-                }
-              else{
-                // if (this.state.successfulCards.includes(i)){
-                //   console.log("Display");
-                //   dis = "None";
-                // }
-                console.log("Flip");
-                return (
-                  <Card 
-                    style={{
-                      width:  this.state.w / this.state.col - 30,
-                      float: "left",
-                      margin: "1%",  
-                      height:  this.state.h / this.state.col - 30                   
-                    }}
-                  >
-                    <CardMedia 
-                      image = {img_link}
-                      className = 'card-image'
-                      />
-                  </Card>)
-              }
-         }
-            )
-        }
+        <div
+          className="grid"
+          style={{
+            width: this.state.w,
+            height: this.state.h
+          }}
+        >
+          {this.state.arr.map((element, i) => {
+            var dis = "";
+            var img_link =
+              "https://deckofcardsapi.com/static/img/" + element + ".png";
+            if (!this.state.isCardFlippedArr[i]) {
+              return (
+                <Card
+                  onClick={() => this.cardClicked(element, i)}
+                  style={{
+                    width: this.state.w / this.state.col - 30,
+                    float: "left",
+                    margin: "1%",
+                    height: this.state.h / this.state.col - 30,
+                    display: dis
+                  }}
+                ></Card>
+              );
+            } else {
+              // if (this.state.successfulCards.includes(i)){
+              //   console.log("Display");
+              //   dis = "None";
+              // }
+              return (
+                <Card
+                  style={{
+                    width: this.state.w / this.state.col - 30,
+                    float: "left",
+                    margin: "1%",
+                    height: this.state.h / this.state.col - 30
+                  }}
+                >
+                  <CardMedia image={img_link} className="card-image" />
+                </Card>
+              );
+            }
+          })}
         </div>
       </div>
     );
